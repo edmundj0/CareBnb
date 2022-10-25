23e95bffbb92156dde3688f3aa67fb8b5ef4a3e2
Edmund Ju <edmundju@berkeley.edu>
2022-10-25 16:40:32 -0700

    GET All Spots complete

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth')
-const { Spot, User, Review, sequelize } = require('../../db/models')
+const { Spot, User, Review, SpotImage, sequelize } = require('../../db/models')
 
 const router = express.Router();
 
@@ -13,9 +13,9 @@ const { handleValidationErrors } = require('../../utils/validation');
 //GET All Spots - return all the spots (require auth - false)
 router.get('/', async (req, res) => {
     const allSpots = await Spot.findAll({
-        include: { model: Review },
+        include: [{ model: Review, attributes: [] }, {model: SpotImage, attributes: []}],
         attributes: {include: [[sequelize.fn("AVG", sequelize.col("Reviews.stars")),
-        "avgRating"]]},
+        "avgRating"], [sequelize.col('SpotImages.url'), 'previewImage']]},
         group: 'Spot.id'
     })
 
