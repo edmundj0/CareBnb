fd66364f4b3c60dc6af1ca177d1c35f334b986c2
Edmund Ju <edmundju@berkeley.edu>
2023-01-23 14:43:58 -0800

    create frontend calendar and start/end date divs

--- frontend/src/components/SpotPage/Bookings/Bookings.js
+++ frontend/src/components/SpotPage/Bookings/Bookings.js
@@ -1,25 +1,52 @@
+import { useState } from 'react';
 import { DateRange, DateRangePicker } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
+import './Bookings.css'
 
 export default function Bookings() {
-
+    const [showCalendar, setShowCalendar] = useState(false)
+    const [startDate, setStartDate] = useState("")
+    const [endDate, setEndDate] = useState("")
 
     //required props for react-date-range
     const selectionRange = {
         startDate: new Date(),
         endDate: new Date(),
         key: 'selection',
-      }
+    }
     const handleSelect = (ranges) => {
         console.log(ranges)
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
     }
 
-    return (
-    <DateRangePicker
-    ranges={[selectionRange]}
-    onChange={handleSelect}
 
-     />
+
+    return (
+        <div>
+            <input
+                placeholder='Choose Start Date'
+                onClick={() => setShowCalendar(showCalendar => !showCalendar)}
+                value={startDate}
+                readOnly>
+            </input>
+            <input
+                placeholder='Choose End Date'
+                onClick={() => setShowCalendar(showCalendar => !showCalendar)}
+                value={endDate}
+                readOnly>
+            </input>
+            {showCalendar &&
+                <div className='booking-calendar-modal'>
+                    <DateRange
+                        ranges={[selectionRange]}
+                        onChange={handleSelect}
+                        showDateDisplay={false}
+                        months={2}
+                    />
+                </div>
+            }
+        </div>
     )
 }
